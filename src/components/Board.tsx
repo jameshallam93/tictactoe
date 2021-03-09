988e0f7e6748b6152f17a2055d56d554c40ba31f
James <james.hallam93@gmail.com>
2021-03-09 12:41:29 +0000

    unstable

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,51 +4,98 @@ import Notification from "./Notification"
 import helper from "./helpers/boardHelper"
 
 
+
+
+
 const Board = () =>{
 
-    const[playersTurn, setPlayersTurn] = useState(true)
-
+    const[turnOf, setTurn] = useState("PLAYER")
     const[board, setBoard] = useState(["","","","","","","","",""])
-
     const[notification, setNotification] = useState("")
 
     const switchTurns = () =>{
-        setPlayersTurn(!playersTurn)
-    }
-
-    const currentSymbol = playersTurn? "X" : "O"
-
-    const setSquareValue = (index:number) =>{
-
-        if (helper.hasDrawn(board)){
-            helper.showNotification(setNotification, "Its a tie! Try again")
-            setTimeout(()=>{
-                resetBoard()
-            },3000)
+        if (turnOf === "CPU"){
+            setTurn("PLAYER")
         }
-
-        if (board[index] === ""){
-            const newBoard = helper.generateNewBoard(board, index, currentSymbol)
-            setBoard(newBoard)
-
-            if (helper.hasWon(newBoard, currentSymbol)){
-                helper.showNotification(setNotification, `${currentSymbol} has won!`)
-                setTimeout(()=>{
-                    resetBoard()
-                },3000)
-                return;}
-
-            switchTurns()
-            }else{
-                helper.showNotification(setNotification, "That square is already taken, please try again")
-            return;
-            }
-        
-
+        if (turnOf === "PLAYER"){
+            setTurn("CPU")
+        }
     }
-    
+    const currentSymbol = turnOf === "PLAYER"? 
+    "X"
+     :
+    "O"
+
     const resetBoard = () =>{
         setBoard(["","","","","","","","",""])
+    }
+
+    const setSquareValue = (board:Array<string>, index:number) =>{
+        const newBoard = helper.generateNewBoard(board, index, currentSymbol)
+        console.log(newBoard)
+        setBoard(newBoard)
+        switchTurns()
+        return newBoard
+    }
+
+    const setPlayerChoice = async (index:number) =>{
+
+        if (helper.hasDrawn(board)){
+
+            helper.showNotification(setNotification, "Its a tie! Try again")
+            helper.timeoutBoardReset(resetBoard)
+        }
+
+        if (board[index] !== ""){
+            helper.showNotification(setNotification, "That square is already taken, please try again")
+            return;
+        }
+        const newBoard = helper.generateNewBoard(board, index, currentSymbol)
+        setBoard(newBoard)
+        
+
+        if (helper.hasWon(newBoard, currentSymbol)){
+                        
+            helper.showNotification(setNotification, `${currentSymbol} has won!`)
+            helper.timeoutBoardReset(resetBoard)
+
+            return
+        }
+
+         switchTurns()
+        console.log(turnOf)
+        setTimeout(()=>{
+            makeBestMove(newBoard)
+        }, 500)
+
+
+
+    }
+
+    const makeBestMove = (board:Array<string>) =>{
+        let bestScore = 1
+        let bestMove = 4
+        for (let i = 0; i < 8; i++){
+            console.log(board[i])
+            if (board[i] === ""){
+                console.log(i);
+                
+                const newBoard = helper.generateNewBoard(board, i, currentSymbol)
+                const score = 1000
+                if (score > bestScore){
+                    bestScore = score
+                    bestMove = i
+                }
+            }
+        }
+        setSquareValue(board, bestMove)
+    }
+    
+    const minimax = () =>{
+
+            return 100
+
+
     }
     
     return (
@@ -68,11 +115,10 @@ const Board = () =>{
             <button onClick = {resetBoard}>reset</button>
         </div>
         <div className = "board">
-            {helper.generateBoard(board, setSquareValue)}
+            {helper.generateBoard(board, setPlayerChoice)}
         </div>
 
         </div>
-
     </>
 )}
 
