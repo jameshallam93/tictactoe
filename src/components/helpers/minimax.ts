c9b0f373c348b74626adcda36679311bf0155939
James <james.hallam93@gmail.com>
2021-03-12 15:23:40 +0000

    change higher order functions

--- src/components/helpers/minimax.ts
+++ src/components/helpers/minimax.ts
@@ -23,7 +23,7 @@ const returnEmptyIndexes = (board:Array<string>):Array<number> =>{
 
     let emptyIndexes:Array<number> = []
 
-    board.map((square:string, index:number)=>{
+    board.forEach((square:string, index:number)=>{
         if (square === ""){
             emptyIndexes.push(index)
         }
@@ -37,29 +37,20 @@ const bestMove =  (board:Array<string>, currentSymbol:string):number => {
     let newBoard = board.slice()
     const emptyIndexes = returnEmptyIndexes(board)
 
-    let bestMove = -1;
     let bestValue = -1000
 
-    //negative scores are not being pushed to the top of the stack
-    emptyIndexes.map((emptyIndex)=>{
-
-        newBoard[emptyIndex] = currentSymbol
-
+    const bestIndex = emptyIndexes.reduce((currentBest, currentIndex):number =>{
+        newBoard[currentIndex] = currentSymbol
         const value = minimax(newBoard, 0, true)
-        if (value === -10){
-
-            
-        }
-        newBoard[emptyIndex] = ""
+        newBoard[currentIndex] = ""
 
         if (value > bestValue){
-            console.log(`new best move: ${emptyIndex} \n new best value: ${value}`)
-            bestMove = emptyIndex
             bestValue = value
+            return currentIndex
         }
+        return currentBest
     })
-
-    return bestMove;
+    return bestIndex
 }
 
 
@@ -81,9 +72,10 @@ const minimax = (board:Array<string>, depth:number, playersTurn:boolean):number 
         return 0;
     }
     if(!playersTurn){
+
         let bestValue = -1000
 
-        board.map((square:string, index:number) =>{
+        board.forEach((square:string, index:number) =>{
             
             let newBoard = board.slice()
 
@@ -103,7 +95,7 @@ const minimax = (board:Array<string>, depth:number, playersTurn:boolean):number 
 
     let bestValue = 1000
         
-    board.map((square:string, index:number) =>{
+    board.forEach((square:string, index:number) =>{
 
         let newBoard = board.slice()
 
