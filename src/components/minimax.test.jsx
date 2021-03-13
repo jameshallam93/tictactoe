89149bbdf93255ce0e1c3ed4c4447a0bfcc8b2c1
James <james.hallam93@gmail.com>
2021-03-13 12:08:19 +0000

    test suite for minimax

--- src/components/minimax.test.jsx
+++ src/components/minimax.test.jsx
@@ -1,14 +1,47 @@
 
 import bestMove from "./helpers/minimax"
+import { minimax, evaluateBoard } from "./helpers/minimax"
 
+//this board will be more thoroughly tested - use for test cases
 let testBoard = [
     "O","","X",
     "","","",
     "","","X"
 ]
 
+let cpuWinBoard = [
+    "O","","X",
+    "","","",
+    "","","X"
+]
+
+let playerWinBoard = [
+    "O", "O", "",
+    "","","",
+    "","",""
+]
+let earlyGameBoard = [
+    "O","","",
+    "","","",
+    "","",""
+]
+let emptyBoard = [
+    "","","",
+    "","","",
+    "","",""
+]
+//to write:
+
+//The following boards:
+    //A board in which the cpu can win in one turn
+    //A board in which the player can win in one turn
+    //An early game board in which there are only two moves the cpu can make to not lose the game
+    //An empty board
+    //An empty board in which the cpu is given three turns
+    //
 
 const cpuMove = bestMove(testBoard, "X")
+
 
 test("best move is",()=>{
     console.log(`bestMove function has given square ${cpuMove+1}`)
@@ -19,7 +52,32 @@ test("best move is",()=>{
     const row3 = testBoard.slice(6,9)
 
     console.log(`\n ${row1} \n ${row2} \n ${row3}`)
+    const score = minimax(testBoard, 0, true)
+    console.log(`With a score of ${score}`);
+    testBoard[cpuMove] = ""
 
 })
-
+describe("The best move function, relying on minimax algorithm chooses correct square in the following scenarios:", ()=>{
+    test("a board in which the cpu can win in one turn", ()=>{
+        expect(bestMove(cpuWinBoard, "X")).toEqual(5)
+    })
+    test("a board in which the player can win in one turn",()=>{
+        expect(bestMove(playerWinBoard, "X")).toEqual(2)
+    })
+    test("an early game board in which the correct move is centre square", ()=>{
+        expect(bestMove(earlyGameBoard, "X")).toEqual(4)
+    })
+    test("an empty board", ()=>{
+        expect(bestMove(emptyBoard, "X")).toEqual(0)
+    })
+    test("given an empty board and THREE turns, will create a winning terminal state", ()=>{
+        const move1 = bestMove(emptyBoard, "X")
+        emptyBoard[move1] = "X"
+        const move2 = bestMove(emptyBoard, "X")
+        emptyBoard[move2] = "X"
+        const move3 = bestMove(emptyBoard, "X")
+        emptyBoard[move3] = "X"
+        expect(evaluateBoard(emptyBoard)).toEqual(10)
+    })
+})
 
